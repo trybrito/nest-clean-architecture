83b2ed3ef4757e467bdecec1b8bc732d9ccdb86a
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-17 22:07:34 -0300

    feat: create account | refactor: cleans unused controllers and modules | chore: validation and rest client

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,46 @@
+import { ConflictException, UsePipes } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+	constructor(private prisma: PrismaService) {}
+
+	@Post()
+	@HttpCode(201)
+	@UsePipes(new ZodValidationPipe(createAccountBodySchema))
+	async handle(@Body() body: CreateAccountBodySchema) {
+		const { name, email, password } = body
+
+		const userWithSameEmail = await this.prisma.user.findUnique({
+			where: {
+				email,
+			},
+		})
+
+		if (userWithSameEmail) {
+			throw new ConflictException('User with same e-mail already exists!')
+		}
+
+		const hashedPassword = await hash(password, 8)
+
+		await this.prisma.user.create({
+			data: {
+				name,
+				email,
+				password: hashedPassword,
+			},
+		})
+	}
+}
---
+++ src/env/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv'
+import { z } from 'zod'
+
+const envSchema = z.object({
+	DATABASE_URL: z.string(),
+})
+
+const env = envSchema.parse(process.env)
+
+export { env }
