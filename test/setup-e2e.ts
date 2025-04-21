1241ca29a9af120cd258ce3bf9364b5c7f633187
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-20 23:44:15 -0300

    chore: create a schema database for e2e tests

---
+++ test/setup-e2e.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseUrl(schemaId: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Please provide a DATABASE_URL via environment variables')
+	}
+
+	const url = new URL(process.env.DATABASE_URL)
+	url.searchParams.set('schema', schemaId)
+
+	return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+	const databaseUrl = generateUniqueDatabaseUrl(schemaId)
+
+	process.env.DATABASE_URL = databaseUrl
+
+	execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+	await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+	await prisma.$disconnect()
+})
