30990e2c8d33b5c41d7a3c5c77977813a7e15e0c
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-20 22:52:11 -0300

    feat: create question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { TokenPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-// const createQuestionBodySchema = z.object({
-// 	title: z.string(),
-// 	content: z.string(),
-// })
+const createQuestionBodySchema = z.object({
+	title: z.string(),
+	content: z.string(),
+})
 
-// type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -17,8 +21,37 @@ export class CreateQuestionController {
 	constructor(private prisma: PrismaService) {}
 
 	@Post()
-	// @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-	async handle(@CurrentUser() user: TokenPayload) {
+	async handle(
+		@CurrentUser() user: TokenPayload,
+		@Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+	) {
+		const userId = user.sub
+		const { title, content } = body
+		const slug = this.convertToSlug(title)
+
+		await this.prisma.question.create({
+			data: {
+				authorId: userId,
+				title,
+				content,
+				slug,
+			},
+		})
+
 		console.log(user)
+	}
+
+	private convertToSlug(text: string) {
+		const slugText = text
+			.normalize('NFKD')
+			.toLowerCase()
+			.trim()
+			.replace(/\s+/g, '-')
+			.replace(/[^\w-]+/g, '')
+			.replace(/_/g, '-')
+			.replace(/--+/g, '-')
+			.replace(/-$/, '')
+
+		return slugText
 	}
 }
