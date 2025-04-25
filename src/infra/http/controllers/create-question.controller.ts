7d0f71d76710cc42f2b502711cc4fbedd830651f
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-24 21:17:46 -0300

    refactor: unify folders according to context

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { CurrentUser } from '@/auth/current-user-decorator'
-import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
-import { TokenPayload } from '@/auth/jwt.strategy'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
-import { PrismaService } from '@/prisma/prisma.service'
+import { CurrentUser } from '../../auth/current-user-decorator'
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
+import { TokenPayload } from '../../auth/jwt.strategy'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { PrismaService } from '../../prisma/prisma.service'
 import { z } from 'zod'
 
 const createQuestionBodySchema = z.object({
