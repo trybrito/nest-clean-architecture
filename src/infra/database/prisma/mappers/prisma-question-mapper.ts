84e62518b2c314da4ad5f66709dc59883122bff1
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-05-04 23:17:53 -0300

    feat: implements prisma questions repository | chore: toPersistency method on question mapper

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-object/slug'
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class PrismaQuestionMapper {
@@ -20,5 +20,20 @@ export class PrismaQuestionMapper {
 			},
 			new UniqueEntityId(raw.id),
 		)
+	}
+
+	static toPersistency(
+		question: Question,
+	): Prisma.QuestionUncheckedCreateInput {
+		return {
+			id: question.id.toString(),
+			authorId: question.authorId.toString(),
+			bestAnswerId: question.bestAnswerId?.toString(),
+			title: question.title,
+			content: question.content,
+			slug: question.slug.value,
+			createdAt: question.createdAt,
+			updatedAt: question.updatedAt,
+		}
 	}
 }
