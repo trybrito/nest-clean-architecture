99e953beeb8504fd5ad6b53dcce4a0a479492326
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-05-09 22:00:52 -0300

    test: upload and create attachment

---
+++ tests/storage/fake-uploader.ts
@@ -0,0 +1,25 @@
+import { randomUUID } from 'node:crypto'
+import {
+	UploadParams,
+	Uploader,
+} from '@/domain/forum/application/storage/uploader'
+
+interface Upload {
+	fileName: string
+	url: string
+}
+
+export class FakeUploader implements Uploader {
+	public uploads: Upload[] = []
+
+	async upload({ fileName }: UploadParams) {
+		const url = randomUUID()
+
+		this.uploads.push({
+			fileName,
+			url,
+		})
+
+		return { url }
+	}
+}
